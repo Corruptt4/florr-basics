c8311ec99a637efc226beb51a41625cbfc63863b
Corruptt4 <corruptt4@gmail.com>
2026-01-25 11:11:06 +0200

    a

--- main.js
+++ main.js
@@ -262,11 +262,11 @@ setInterval(() => {
                 if (collider1.type == "player" && collider2.type == "mob" && collider2.pet) return;
                 if (collider2.type == "player" && collider1.type == "mob" && collider1.pet) return;
 
-                collider2.push.x += 10*Math.max((collider1.mass/collider2.mass) * Math.log(collider2.mass), 1) * Math.cos(angle)
-                collider2.push.y += 10*Math.max((collider1.mass/collider2.mass) * Math.log(collider2.mass), 1) * Math.sin(angle)
+                collider2.push.x += 8*Math.max(1, (collider1.mass/collider2.mass)) * Math.cos(angle)
+                collider2.push.y += 8*Math.max(1, (collider1.mass/collider2.mass)) * Math.sin(angle)
                 
-                collider1.push.x -= 10*Math.max((collider2.mass/collider1.mass) * Math.log(collider1.mass), 1) * Math.cos(angle)
-                collider1.push.y -= 10*Math.max((collider2.mass/collider1.mass) * Math.log(collider1.mass), 1) * Math.sin(angle)
+                collider1.push.x -= 8*Math.max(1, (collider2.mass/collider1.mass)) * Math.cos(angle)
+                collider1.push.y -= 8*Math.max(1, (collider2.mass/collider1.mass)) * Math.sin(angle)
 
                 if ((collider1.type == "mob" && !collider1.pet) && (collider2.type == "mob" && collider2.pet)) {
                     collider1.health -= collider2.damage
